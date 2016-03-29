df74c36a05dfa8a8ff6b3596c226ad4c6946f690
kimufly <kimufly@live.com>
2016-03-29 14:09:23 +0800

    hh

---
+++ zsh/hh_rc.zsh
@@ -0,0 +1,4 @@
+# https://github.com/dvorka/hstr
+export HISTFILE=~/.zsh_history  # ensure history file visibility
+export HH_CONFIG=hicolor        # get more colors
+bindkey -s "\C-r" "\eqhh\n"     # bind hh to Ctrl-r (for Vi mode check doc)
